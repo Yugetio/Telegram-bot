edd87017263eb703ae257b5140aeef4bb21b76f8
Vladyslav Kaminskyi <yugetio@gmail.com>
2018-02-20 17:16:08 +0200

    message operations (forward, reply, edit, delete)

--- index.js
+++ index.js
@@ -182,3 +182,64 @@ bot.on('callback_query', query => {
   });
 
 }); */
+
+
+
+
+// работа с сообщениями
+const inline_keyboard = [
+  [{
+    text: 'Forward',
+    callback_data: 'forward'
+  }, {
+    text: 'Reply',
+    callback_data: 'reply'
+  }],
+  [{
+    text: 'Edit',
+    callback_data: 'edit'
+  }, {
+    text: 'Delete',
+    callback_data: 'delete'
+  }]
+];
+
+bot.on('callback_query', query => {
+  const { chat, message_id, text } = query.message;
+
+  switch (query.data) {
+    case 'forward':
+      //куда, откуда, что
+      bot.forwardMessage(chat.id, chat.id, message_id);
+      break;
+    case 'reply':
+      bot.sendMessage(chat.id, "отвечаю на сообщение", {
+        reply_to_message_id: message_id
+      });
+      break;
+    case 'edit':
+      bot.editMessageText(`${text} (edited)`, {
+        chat_id: chat.id,
+        message_id: message_id,
+        reply_markup: { inline_keyboard }
+      });
+      break;
+    case 'delete':
+      bot.deleteMessage(chat.id, message_id);
+      break;
+  }
+
+  bot.answerCallbackQuery({
+    callback_query_id: query.id
+  });
+});
+
+bot.onText(/\/start/, (msg, [source, match]) => {
+  const chatID = msg.chat.id;
+
+  bot.sendMessage(chatID, 'Keybiard', {
+    reply_markup: {
+      inline_keyboard
+    }
+  });
+});
