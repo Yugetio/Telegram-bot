bd01075916628999fbfcd26b1f4a445fbeb224ce
Vladyslav Kaminskyi <yugetio@gmail.com>
2018-02-21 11:34:51 +0200

    added keyboard

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 const helper = require('./helper');
+const keyboard = require('./keyboard');
+const kb = require('./keyboard-btn');
 
 const bot = new TelegramBot(config.TOKEN, {
   polling: true
@@ -9,6 +11,25 @@ const bot = new TelegramBot(config.TOKEN, {
 
 helper.logStart();
 
+bot.onText(/\/start/, msg => {
+  const text = `Здравствуйте, ${msg.from.first_name}\nВыберите команду для начала работы`;
+  bot.sendMessage(helper.getChatId(msg), text, {
+    reply_markup: {
+      keyboard: keyboard.home
+    }
+  });
+});
+
 bot.on('message', msg => {
-  console.log('Working!');
+  switch (msg.text) {
+    case kb.home.favourite:
+      ;
+      break;
+    case kb.home.films:
+      ;
+      break;
+    case kb.home.cinemas:
+      ;
+      break;
+  }
 });
---
+++ src/keyboard.js
@@ -0,0 +1,10 @@
+const kb = require('./keyboard-btn');
+module.exports = {
+  home: [
+    [kb.home.films, kb.home.cinemas],
+    [kb.home.favourite]
+  ],
+  films: [
+
+  ]
+};
